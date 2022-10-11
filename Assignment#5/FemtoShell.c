f39a88d04897dd3fdfb7afe0283ab0abe0355ae4
Mazen-ibrahim <mkandlo@icloud.com>
2022-10-11 23:19:18 +0200

    add unset command

--- Assignment#5/FemtoShell.c
+++ Assignment#5/FemtoShell.c
@@ -4,6 +4,9 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define export 1
+#define unset  2
+#define fail   0
 
 
 /************************************************************************
@@ -12,8 +15,8 @@
 *************************************************************************/
 struct data
 {
-   char name[20];
-   char value[20];
+  char name[20];
+  char value[20];
 };
 
 
@@ -26,19 +29,20 @@ struct data
 	 3-delimeter that used by built in function strtok() used to parse data by it   *
                                                                                         *
 *****************************************************************************************/
-void parse(char input[], char *output[],char delim[])
+
+void parse (char input[], char *output[], char delim[])
 {
 
-	char *ptr;
-       	ptr = strtok(input, delim);
-	int k = 0;
-        while(ptr != NULL)
-	{
-		output[k] = ptr;
-		ptr = strtok(NULL, delim);
-		k++;
-	}
-	output[k]=NULL;
+  char *ptr;
+  ptr = strtok (input, delim);
+  int k = 0;
+  while (ptr != NULL)
+    {
+      output[k] = ptr;
+      ptr = strtok (NULL, delim);
+      k++;
+    }
+  output[k] = NULL;
 
 }
 
@@ -50,15 +54,17 @@ void parse(char input[], char *output[],char delim[])
 	                                                                                *
 *****************************************************************************************/
 
-int checkEqual(char input[])
+int checkEqual (char input[])
 {
-	for(int i = 0 ; i <strlen(input); i++)
+  for (int i = 0; i < strlen (input); i++)
+    {
+      if (input[i] == '=')
 	{
-		if(input[i] == '=')
-		  {return i;}
+	  return i;
 	}
+    }
 
-       return 0;
+  return 0;
 }
 
 
@@ -70,145 +76,174 @@ int checkEqual(char input[])
                                                                                         *
 *****************************************************************************************/
 
-int checkExport (char input[])
+int check (char input[])
 {
-     if ( input[0] == 'e' && input[1] == 'x' && input[2] == 'p' && input[3] == 'o' && input[4] == 'r' && input[5] == 't')
-           return 1;
-     else
-	   return 0;     
+  if (input[0] == 'e' && input[1] == 'x' && input[2] == 'p' && input[3] == 'o'
+      && input[4] == 'r' && input[5] == 't')
+    return export;
+  if (input[0] == 'u' && input[1] == 'n' && input[2] == 's' && input[3] == 'e'
+      && input[4] == 't')
+    return unset;
+  else
+    return fail;
 }
 
 
-int main()
-{ 
-  
+int main ()
+{
+
   int counter_variables = 0;
   int Export;
   int equal;
   int j;
   struct data variables[20];
-  int id , status , ret;
+  int id, status, ret;
   char Buf[100];
   char *output[20];
 
-  while(1)
-  {
+  while (1)
+    {
 
-    printf("Ana Gahez ya Basha > ");
-    fgets(Buf,100,stdin);
-    Buf[strlen(Buf)-1] = 0;
-        
-
-         if( strlen(Buf) == 0 )
-	      continue;
-             
+      printf ("Ana Gahez ya Basha > ");
+      fgets (Buf, 100, stdin);
+      Buf[strlen (Buf) - 1] = 0;
 
 
-
-	 //Set command code section
-      	 //-----------------------------------------------------------------------------------------------------------
-	
-	 if( strcmp(Buf,"set") == 0 )
-         {
-             
-            for(int i = 0 ; i<counter_variables; i++)
-            {
-                 printf("Local variable[%d] is %s = %s \n", i+1 ,  variables[i].name , variables[i].value);
-            }
-	    continue;
-
-         }
-         
-         //-----------------------------------------------------------------------------------------------------------
+      if (strlen (Buf) == 0)
+	continue;
 
 
-         //Export code section 
-         //---------------------------------------------------------------------------------------------------------
 
-            Export = checkExport(Buf);
 
-         if ( Export == 1 )
-         {
-            char found = 0;
-            char success = -1;
-            parse(Buf,output," ");
-            for(int i = 0 ;i<counter_variables; i++)
-             {
-                  if ( strcmp(output[1] , variables[i].name) == 0 )
-                    {
-                       found = 1;
-                       success = setenv(variables[i].name,variables[i].value,1);
-                       if( success == 1 )
-                               printf("Variable set to enviroment variables\n");
+      //Set command code section
+      //-----------------------------------------------------------------------------------------------------------
 
-                        break;
-                    }
-             }
+      if (strcmp (Buf, "set") == 0)
+	{
 
-           if(found == 0)
-               printf("Variable not found\n");
+	  for (int i = 0; i < counter_variables; i++)
+	    {
+	      printf ("Local variable[%d] is %s = %s \n", i + 1,
+		      variables[i].name, variables[i].value);
+	    }
+	  continue;
 
-            continue;
-         }
+	}
 
-         //----------------------------------------------------------------------------------------------------------
+      //-----------------------------------------------------------------------------------------------------------
 
-         
-	 //Add variable code section
-	 //-----------------------------------------------------------------------------------------------------------
-        
-	   equal = checkEqual(Buf);
-           j = 0;
 
-	 if ( equal > 0)
-         {
-            for(int i = 0 ;i<strlen(Buf); i++)
-	    {  
-              if( i < equal ) 
-	      {
-		   variables[counter_variables].name[i] = Buf[i];
-	      }
+      //Export code section 
+      //---------------------------------------------------------------------------------------------------------
+
+      if (check (Buf) == export)
+	{
+	  char found = 0;
+	  char success = -1;
+	  parse (Buf, output, " ");
+	  for (int i = 0; i < counter_variables; i++)
+	    {
+	      if (strcmp (output[1], variables[i].name) == 0)
+		{
+		  found = 1;
+		  success = setenv (variables[i].name, variables[i].value, 1);
+		  if (success == 1)
+		    printf ("Variable set to enviroment variables\n");
+
+		  break;
+		}
+	    }
+
+	  if (found == 0)
+	    printf ("Variable not found\n");
+
+	  continue;
+	}
+
+      //----------------------------------------------------------------------------------------------------------
+
+
+      //Unset command
+      //----------------------------------------------------------------------------------------------------------
+
+      if (check (Buf) == unset)
+	{
+	  char found = 0;
+	  parse (Buf, output, " ");
+	  for (int i = 0; i < counter_variables; i++)
+	    {
+	      if (strcmp (output[1], variables[i].name) == 0)
+		{
+		  found = 1;
+		  unsetenv (variables[i].name);
+
+		  break;
+		}
+	    }
+
+	  if (found == 0)
+	    printf ("Variable not found\n");
+
+	  continue;
+
+	}
+
+
+
+      //Add variable code section
+      //-----------------------------------------------------------------------------------------------------------
+
+      equal = checkEqual (Buf);
+      j = 0;
+
+      if (equal > 0)
+	{
+	  for (int i = 0; i < strlen (Buf); i++)
+	    {
+	      if (i < equal)
+		{
+		  variables[counter_variables].name[i] = Buf[i];
+		}
 	      else
-	      {  
-                 variables[counter_variables].name[i] = '\0';
-	       	 variables[counter_variables].value[j]  = Buf[i+1];
-		 j++; 
-	      }
+		{
+		  variables[counter_variables].name[i] = '\0';
+		  variables[counter_variables].value[j] = Buf[i + 1];
+		  j++;
+		}
 
 	    }
-         
-            variables[counter_variables].value[j] = '\0'; 
-            counter_variables++;
-	    continue;
-         }
 
-	 //------------------------------------------------------------------------------------------------------------
+	  variables[counter_variables].value[j] = '\0';
+	  counter_variables++;
+	  continue;
+	}
+
+      //------------------------------------------------------------------------------------------------------------
 
 
-    id = fork();
+      id = fork ();
 
-    if( id < 0 )
-	printf("Fork Failed\n");
-    
-    else if ( id > 0 )   //Parent process
-    {  
-       
-       wait(&status);   	    
+      if (id < 0)
+	printf ("Fork Failed\n");
+
+      else if (id > 0)		//Parent process
+	{
+
+	  wait (&status);
+	}
+
+      else if (id == 0)		//child process
+	{
+
+	  parse (Buf, output, " ");
+	  ret = execvp (output[0], output);
+	  if (ret == -1)
+	    printf ("Failed to exec\n");
+
+	}
+
+
     }
-
-    else if (id == 0 )   //child process
-    {      
-	   printf("%s\n",Buf);
-           parse(Buf,output," ");
-           ret = execvp(output[0],output);
-           if ( ret == -1 )
-	     printf("Failed to exec\n"); 
-	
-     }
-
-                 
-  }
 
 
 }
-
